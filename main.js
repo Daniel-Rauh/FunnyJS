3f9bd90ae597bdf209ca613aa47390b4f53a0220
Daniel-Rauh <daniel.rauh@gmail.com>
2020-06-16 16:12:00 +0200

    Bootsy

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 let x = 0
-document.getElementById('result').innerHTML="<h2 id='red'>Rute stinkt!</h2>"
+document.getElementById('result').innerHTML="<h2 id='red'>Muss auf jeden Fall nochmal!</h2>"
 function minusOne(){
     x--
     document.getElementById("num").innerHTML = "<h3>" + x + "</h3>"
-    if (x < 3) {
-        document.getElementById('result').innerHTML="<h2 id='red'>Rute stinkt!</h2>"
-    } else if (x < 5) {
-        document.getElementById('result').innerHTML="<h2 id='blue'>Rute stinkt vielleicht!</h2>"
+    if (x < 4) {
+        document.getElementById('result').innerHTML="<h2 id='red'>Muss auf jeden Fall nochmal!</h2>"
+    } else if (x < 6) {
+        document.getElementById('result').innerHTML="<h2 id='blue'>Könnte noch was kommen!</h2>"
     } else {
-        document.getElementById('result').innerHTML="<h2 id='green'>Rute stinkt nicht!</h2>"
+        document.getElementById('result').innerHTML="<h2 id='green'>Bootsy ist leer!</h2>"
     }
 }
 function plusOne() {
     x++
     document.getElementById("num").innerHTML = "<h3>" + x + "</h3>"
-    if (x < 3) {
-        document.getElementById('result').innerHTML="<h2 id='red'>Rute stinkt!</h2>"
-    } else if (x > 3, x < 5) {
-        document.getElementById('result').innerHTML="<h2 id='blue'>Rute stinkt vielleicht!</h2>"
+    if (x < 4) {
+        document.getElementById('result').innerHTML="<h2 id='red'>Muss auf jeden Fall nochmal!</h2>"
+    } else if (x < 6) {
+        document.getElementById('result').innerHTML="<h2 id='blue'>Könnte noch was kommen!</h2>"
     } else {
-        document.getElementById('result').innerHTML="<h2 id='green'>Rute stinkt nicht!</h2>"
+        document.getElementById('result').innerHTML="<h2 id='green'>Bootsy ist leer!</h2>"
     }
 }
